8b64423a6fe9e2f3d0356f7c53c8b1597e586b5e
Ezekiel Yu <cellozeke@gmail.com>
2021-07-30 14:11:47 -0700

    finished project setup. starting user auth

---
+++ frontend/amanom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener('DOMContentLoaded', () => {
+  // const store = 
+  const root = document.getElementById('root')
+  ReactDOM.render(<h1>Welcome to Amanom!</h1>, root)
+})
+
