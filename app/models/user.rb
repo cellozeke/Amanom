436f5094686dcb8bfea6b4f3e2bdade56cb7a238
Ezekiel Yu <cellozeke@gmail.com>
2021-07-30 15:30:25 -0700

    finished user model, application controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,43 @@
 class User < ApplicationRecord
+  validates :username,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 3, maximum: 20 },
+    format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :email,
+    presence: true,
+    uniqueness: { case_sensitive: false },
+    length: { minimum: 6, maximum: 255 },
+    format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i }
+  validates :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6 }, allow_nil: true
+
+  after_initialize :ensure_session_token
+
+  attr_reader :password
+
+  def self.find_by_credentials(username_or_email, password)
+    user = User.find_by(username: username_or_email) || User.find_by(email: username_or_email)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom.urlsafe_base64
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64
+  end
 end
