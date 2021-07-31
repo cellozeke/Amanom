221d135f158ffa91aa2bc4891e79e462a90b2c9f
Ezekiel Yu <cellozeke@gmail.com>
2021-07-30 17:53:10 -0700

    added users and sessions controllers, routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   root to: 'root#root'
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: :create
+    resource :session, only: %i[create destroy]
+  end
 end
