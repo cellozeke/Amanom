ae22e01c2d827d921512dff390f8e4b2db83e75d
Ezekiel Yu <cellozeke@gmail.com>
2021-08-01 18:55:42 -0700

    added App and Root components

--- frontend/amanom.jsx
+++ frontend/amanom.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import configureStore from './store/store'
+import Root from './components/root'
 
 import { sign_up, log_in, log_out } from './actions/session_actions'
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore()
   const root = document.getElementById('root')
-  ReactDOM.render(<h1>Welcome to Amanom!</h1>, root)
+  ReactDOM.render(<Root store={store} />, root)
 
   window.sign_up = sign_up
   window.log_in = log_in
   window.log_out = log_out
+  window.store = store
   window.dispatch = store.dispatch
   window.getState = store.getState
 })
