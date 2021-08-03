ecc31ba2315e1fa0ce33f65128f2ca33154e0d5c
Ezekiel Yu <cellozeke@gmail.com>
2021-08-03 14:32:51 -0700

    added most of login page styling

--- frontend/components/session_forms/login_form.jsx
+++ frontend/components/session_forms/login_form.jsx
@@ -25,19 +25,26 @@ export default class LoginForm extends React.Component {
   render() {
     const { errors } = this.props
     return (
-      <div>
-        <h2 className='log-in'>Log In</h2>
-        <form onSubmit={this.handleSubmit}>
-          <label>Username or Email: 
-            <input type="text" value={this.state.usernameOrEmail} onChange={this.handleInput('usernameOrEmail')} />
-          </label>
-          <label>Password: 
-            <input type="password" value={this.state.password} onChange={this.handleInput('password')} />
-          </label>
-          {errors.map(error => <p key={errors.indexOf(error)}>{error}</p>)}
-          <input type="submit" value="Log In" />
-        </form>
-        <Link to='/signup'>Sign Up</Link>
+      <div className='login-container'>
+        <div className='login-div'>
+          <h2 className='login-header'>Log In</h2>
+          <form className='login-form' onSubmit={this.handleSubmit}>
+            <label className='login-cred-1-text'>Username or Email
+              <input className='login-cred-1-input' type="text" value={this.state.usernameOrEmail} onChange={this.handleInput('usernameOrEmail')} />
+            </label>
+            <label className='login-cred-2-text'>Password
+              <input className='login-cred-2-input' type="password" value={this.state.password} onChange={this.handleInput('password')} />
+            </label>
+            <input className='login-submit' type="submit" value="Continue" />
+            {errors.map(error => <p className='login-error-message' key={errors.indexOf(error)}>{error}</p>)}
+          </form>
+        </div>
+        <div className='login-signup-link-div'>
+          <p className='login-signup-text'>New to Amanom?</p>
+          <div className='login-signup-text-strikethrough'> </div>
+          <Link className='login-signup-link' to='/signup'>Create your Amanom account</Link>
+        </div>
+        <div className='login-signup-bottom-divider'> </div>
       </div>
     )
   }
