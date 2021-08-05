4352f8bc597870f60bf19ae5c602c97482a23050
Ezekiel Yu <cellozeke@gmail.com>
2021-08-05 11:37:50 -0700

    tidied up navbar

--- frontend/components/nav_bar/nav_bar.jsx
+++ frontend/components/nav_bar/nav_bar.jsx
@@ -12,32 +12,34 @@ export default class NavBar extends React.Component {
     return (
       <div className='nav-bar'>
         <div className='nav-left'>
-          <Link className='nav-logo-button' to='/' >
+          <Link className='nav-logo-button nav-sub' to='/' >
             <img className='nav-logo' src="images/amanom-logo.png"/>
           </Link>
-          <div className='nav-greeting-div'>
-            <p className='nav-text-normal'>Hello,</p>
+          <div className='nav-greeting-div nav-sub'>
+            <p className='nav-text-normal'>{currentUser ? 'Hello,' : 'Welcome,'}</p>
             <p className='nav-text-bold'>{currentUser ? currentUser.username : 'Guest'}</p>
           </div>
         </div>
+
         <div className='nav-center'>
 
         </div>
+
         <div className='nav-right'>
           {currentUser ?
-            <div className='nav-orders-div'>
-              <p className='nav-text-normal'>Your</p>
+            <div className='nav-profile-div nav-sub'>
+              <p className='nav-text-normal'>{'Reviews &'}</p>
               <p className='nav-text-bold'>Orders</p>
             </div>
             :
-            <Link className='nav-sign-up-div nav-text-bold' to='/signup'>Sign Up</Link>
+            <Link className='nav-sign-up-div nav-sub nav-text-bold' to='/signup'>Sign Up</Link>
           }
           {currentUser ?
-            <div className='nav-log-out-div nav-text-bold' onClick={this.logOut}>Log Out</div>
+            <div className='nav-log-out-div nav-sub nav-text-bold' onClick={this.logOut}>Log Out</div>
             :
-            <Link className='nav-log-in-div nav-text-bold' to='/login'>Log In</Link>
+            <Link className='nav-log-in-div nav-sub nav-text-bold' to='/login'>Log In</Link>
           }
-          <div className='nav-cart-div'>
+          <div className='nav-cart-div nav-sub'>
             <img className='nav-cart-img' src="images/amanom-cart.png"/>
           </div>
         </div>
