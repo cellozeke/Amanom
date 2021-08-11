de3e4e2242d0edb3b5038069e4b086e7e71e6c06
Ezekiel Yu <cellozeke@gmail.com>
2021-08-11 14:38:34 -0700

    finished categories bar more or less

--- frontend/components/categories_bar/categories_bar.jsx
+++ frontend/components/categories_bar/categories_bar.jsx
@@ -4,7 +4,13 @@ import { Link } from 'react-router-dom'
 export const CategoriesBar = (props) => (
   <div className='categories-bar-main-div'>
     <Link to='/search?q='>All</Link>
+    <Link to='/search?q=chips'>Chips</Link>
+    <Link to='/search?q=chocolate%20candy'>{'Chocolate & Candy'}</Link>
     <Link to='/search?q=cookies'>Cookies</Link>
-
+    <Link to='/search?q=cracker%20pretzel'>{'Crackers & Pretzels'}</Link>
+    <Link to='/search?q=fruit%20snacks'>Fruit Snacks</Link>
+    <Link to='/search?q=jerky'>Jerky</Link>
+    <Link to='/search?q=popcorn'>Popcorn</Link>
+    <Link to='/search?q=nuts'>Nuts</Link>
   </div>
 )
