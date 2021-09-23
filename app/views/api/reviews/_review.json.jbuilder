8220e3b9600a1b9864cf42adb6032e53b00f4dea
Ezekiel Yu <cellozeke@gmail.com>
2021-09-23 15:24:57 -0700

    added basic review history layout

--- app/views/api/reviews/_review.json.jbuilder
+++ app/views/api/reviews/_review.json.jbuilder
@@ -1,2 +1,4 @@
 json.extract! review, :id, :user_id, :snack_id, :title, :body, :stars, :updated_at
+json.snackName review.snack.name
+json.photoUrl url_for(review.snack.photo)
 json.username review.user.username
--- frontend/components/profile/review_history.jsx
+++ frontend/components/profile/review_history.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default class ReviewHistory extends React.Component {
   render() {
+    let { reviews } = this.props
+
+    if (!reviews.length) return (
+      <div className='review-history-empty'>
+        No reviews
+      </div>
+    )
+
     return (
-      <div>
-        {this.props.reviews.map(review => review.id)}
+      <div className='review-history-main-div'>
+        {reviews.map((review, idx) =>
+          <div className='review-history-review' key={idx}>
+            <Link className='review-history-review-link' to={`/snacks/${review.snackId}`}>
+              <img className='review-history-review-img' src={review.photoUrl} />
+            </Link>
+            <div className='review-history-review-info'>
+              <Link className='review-history-review-name' to={`/snacks/${review.snackId}`}>{review.snackName}</Link>
+              <div className='stars' style={{'--rating': `${review.stars}`}} ></div>
+              <p>{review.title}</p>
+              <p>{review.body}</p>
+              <p>{review.updatedAt}</p>
+            </div>
+          </div>
+        )}
       </div>
     )
   }
