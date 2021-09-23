294ffd3f7215cb614363727f117413ae161fe2b2
Ezekiel Yu <cellozeke@gmail.com>
2021-09-23 14:02:18 -0700

    fixed review edit form

--- frontend/components/snack/snack_review.jsx
+++ frontend/components/snack/snack_review.jsx
@@ -4,21 +4,67 @@ import SnackReviewEditForm from './snack_review_edit_form'
 export default class SnackReview extends React.Component {
   constructor(props) {
     super(props)
+    let { review } = this.props
     this.state = {
-      editing: false
+      editing: false,
+      id: review.id,
+      stars: review.stars,
+      title: review.title,
+      body: review.body,
+      userId: review.userId,
+      snackId: review.snackId
     }
   }
 
   handleClick = e => {
+    e.preventDefault()
     this.setState({editing: true})
+  }
+
+  handleCancel = e => {
+    e.preventDefault()
+    this.setState({editing: false})
+  }
+
+  handleChange = field => e => {
+    e.preventDefault()
+    this.setState({[field]: e.target.value})
+  }
+
+  handleStars = e => {
+    e.preventDefault()
+    this.setState({stars: parseInt(e.target.id)})
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    if (!this.state.stars) console.log('fix this')
+    console.log(this.state)
+
+    this.props.updateSnackReview(this.state)
+      .then(res => this.props.fetchSnack(this.props.snackId))
+      .then(res => this.setState({editing: false}))
   }
 
   render() {
     const { review } = this.props
-    console.log(review)
 
     if (this.state.editing) return (
-      <SnackReviewEditForm review={review} updateSnackReview={this.props.updateSnackReview} fetchSnack={this.props.fetchSnack} snackId={this.props.snackId}/>
+      <form className='snack-review-form-main-div' onSubmit={this.handleSubmit}>
+        <p className='snack-review-form-header'>Edit your review</p>
+        <div className='snack-review-form-stars-div'>
+          <p className={`snack-review-form-star star-1 ${this.state.stars >= 1 ? 'filled' : ''}`} id={1} onClick={this.handleStars}>★</p>
+          <p className={`snack-review-form-star star-2 ${this.state.stars >= 2 ? 'filled' : ''}`} id={2} onClick={this.handleStars}>★</p>
+          <p className={`snack-review-form-star star-3 ${this.state.stars >= 3 ? 'filled' : ''}`} id={3} onClick={this.handleStars}>★</p>
+          <p className={`snack-review-form-star star-4 ${this.state.stars >= 4 ? 'filled' : ''}`} id={4} onClick={this.handleStars}>★</p>
+          <p className={`snack-review-form-star star-5 ${this.state.stars >= 5 ? 'filled' : ''}`} id={5} onClick={this.handleStars}>★</p>
+        </div>
+        <input className='snack-review-form-title' onChange={this.handleChange('title')} type="text" minLength='1' maxLength='63' placeholder='Title' defaultValue={this.state.title}/>
+        <textarea className='snack-review-form-body' onChange={this.handleChange('body')} type="text" minLength='1' placeholder='Body' defaultValue={this.state.body}></textarea>
+        <input type="submit" value="Submit" />
+        <button onClick={this.handleCancel}>Cancel</button>
+      </form>
+      // <SnackReviewEditForm review={review} updateSnackReview={this.props.updateSnackReview} fetchSnack={this.props.fetchSnack} snackId={this.props.snackId}/>
       // null
     )
 
--- frontend/components/snack/snack_review_form.jsx
+++ frontend/components/snack/snack_review_form.jsx
@@ -17,7 +17,7 @@ export default class SnackReviewForm extends React.Component {
     this.setState({[field]: e.target.value})
   }
 
-  handleClick = e => {
+  handleStars = e => {
     e.preventDefault()
     this.setState({stars: parseInt(e.target.id)})
   }
@@ -37,11 +37,11 @@ export default class SnackReviewForm extends React.Component {
         <form className='snack-review-form-main-div' onSubmit={this.handleSubmit}>
           <p className='snack-review-form-header'>Write a review!</p>
           <div className='snack-review-form-stars-div'>
-            <p className={`snack-review-form-star star-1 ${this.state.stars >= 1 ? 'filled' : ''}`} id={1} onClick={this.handleClick}>★</p>
-            <p className={`snack-review-form-star star-2 ${this.state.stars >= 2 ? 'filled' : ''}`} id={2} onClick={this.handleClick}>★</p>
-            <p className={`snack-review-form-star star-3 ${this.state.stars >= 3 ? 'filled' : ''}`} id={3} onClick={this.handleClick}>★</p>
-            <p className={`snack-review-form-star star-4 ${this.state.stars >= 4 ? 'filled' : ''}`} id={4} onClick={this.handleClick}>★</p>
-            <p className={`snack-review-form-star star-5 ${this.state.stars >= 5 ? 'filled' : ''}`} id={5} onClick={this.handleClick}>★</p>
+            <p className={`snack-review-form-star star-1 ${this.state.stars >= 1 ? 'filled' : ''}`} id={1} onClick={this.handleStars}>★</p>
+            <p className={`snack-review-form-star star-2 ${this.state.stars >= 2 ? 'filled' : ''}`} id={2} onClick={this.handleStars}>★</p>
+            <p className={`snack-review-form-star star-3 ${this.state.stars >= 3 ? 'filled' : ''}`} id={3} onClick={this.handleStars}>★</p>
+            <p className={`snack-review-form-star star-4 ${this.state.stars >= 4 ? 'filled' : ''}`} id={4} onClick={this.handleStars}>★</p>
+            <p className={`snack-review-form-star star-5 ${this.state.stars >= 5 ? 'filled' : ''}`} id={5} onClick={this.handleStars}>★</p>
           </div>
           <input className='snack-review-form-title' onChange={this.handleChange('title')} type="text" minLength='1' maxLength='63' placeholder='Title'/>
           <textarea className='snack-review-form-body' onChange={this.handleChange('body')} type="text" minLength='1' placeholder='Body'></textarea>
