5e80a7174024bc063cf4ce10c23c46896bd5a8ed
Ezekiel Yu <cellozeke@gmail.com>
2021-10-01 15:16:23 -0700

    finished quantity css maybe

--- frontend/components/snack/snack_order_item.jsx
+++ frontend/components/snack/snack_order_item.jsx
@@ -4,7 +4,6 @@ import { moneyFormatter } from '../../utils/extra_utils'
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-import { fontSize, width } from '@mui/system';
 
 export default class SnackOrderItem extends React.Component {
   handleQuantity = e => {
@@ -35,31 +34,18 @@ export default class SnackOrderItem extends React.Component {
         <p className='snack-order-availability'>In Stock</p>
         {currentUser ? <div className='snack-order-quantity-div'>
           <p className='snack-order-quantity-text'>Qty: </p>
-          <FormControl size='small'>
+          <FormControl >
             <Select
               value={cartItem.matchingItem.quantity}
               onChange={this.handleQuantity}
-              sx={{height: 24, width: 56, fontSize: 14}}
+              sx={{height: 24, width: 56, fontSize: 14, padding: 0}}
             >
               {[...Array(11).keys()].map(quantity =>
                 <MenuItem key={quantity} value={quantity}> {quantity} </MenuItem>
               )}
             </Select>
           </FormControl>
-          {/* <select className='snack-order-quantity-select' value={cartItem.matchingItem.quantity} onChange={this.handleQuantity}>
-            <option value={0}>0</option>
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-            <option value={6}>6</option>
-            <option value={7}>7</option>
-            <option value={8}>8</option>
-            <option value={9}>9</option>
-            <option value={10}>10</option>
-          </select> */}
-        </div> : <div></div>}
+        </div> : null}
         <Link className='button' to='/cart' >Go to cart</Link>
         <div className='snack-order-secure'><img className='snack-order-secure-img' src="/images/secure.png" />Secure transaction</div>
         <div className='snack-order-ship-sell-text'>Ships from <p className='snack-order-shipper'>AmanomFresh</p></div>
