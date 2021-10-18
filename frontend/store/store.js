6910721dcba29e5c77bb1968fc9f130f809313ab
Ezekiel Yu <cellozeke@gmail.com>
2021-10-18 15:29:14 -0700

    removed logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -1,9 +1,10 @@
 import { applyMiddleware, createStore, compose  } from "redux"
-import logger from "redux-logger"
+// import logger from "redux-logger"
 import thunk from "../middleware/thunk"
 import rootReducer from '../reducers/root_reducer'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const configureStore = (preloadedState={}) => createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk, logger)))
+const configureStore = (preloadedState={}) => createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk)))
+// const configureStore = (preloadedState={}) => createStore(rootReducer, preloadedState, composeEnhancers(applyMiddleware(thunk, logger)))
 
 export default configureStore
